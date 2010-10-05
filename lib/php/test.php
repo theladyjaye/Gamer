f29c0a1ec468ffe7f4ba53f288b1b95f102adb75
aventurella <aventurella@gmail.com>
2010-10-05 14:31:58 -0700

    better test output

--- lib/php/test.php
+++ lib/php/test.php
@@ -3,18 +3,17 @@ session_start();
 require 'GMRClient.php';
 $client = new GMRClient('12345');
 ?>
-
 <h2>Schedules Matched for bpuglisi</h2>
 <pre>
-<?php print_r($client->matchesForUser('bpuglisi')); ?>
+	<?php print_r($client->matchesForUser('bpuglisi')); ?>
 </pre>
-
+<hr>
 <h2>Games for XBox 360 <span style="color:#00cc00">Page 1</span></h2>
 <pre>
-<?php print_r($client->gamesForPlatform(GMRPlatform::kXbox360));?>
+	<?php print_r($client->gamesForPlatform(GMRPlatform::kXbox360));?>
 </pre>
-
+<hr>
 <h2>Games for XBox 360 <span style="color:#00cc00">Page 2</span></h2>
 <pre>
-<?php print_r($client->gamesForPlatform(GMRPlatform::kXbox360, "game/halo-reach"));?>
+	<?php print_r($client->gamesForPlatform(GMRPlatform::kXbox360, "game/halo-reach"));?>
 </pre>
