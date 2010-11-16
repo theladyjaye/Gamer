223de6ce06c745a5b23045e1ab8381a4d026c19e
aventurella <aventurella@gmail.com>
2010-11-16 08:39:36 -0800

    Game Detail View Assets and prelim xib setup

---
+++ lib/ios/Gamer/GMRGameDetailController.h
@@ -0,0 +1,16 @@
+//
+//  GMRGameDetailController.h
+//  Gamer
+//
+//  Created by Adam Venturella on 11/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GMRGameDetailController : UIViewController {
+
+}
+
+@end
