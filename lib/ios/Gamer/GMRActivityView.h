731b6d74b66ac2b44d97adcdbcdb0912dc007e66
Adam Venturella <aventurella@gmail.com>
2010-12-08 07:48:28 -0800

    Profile Comp, Activity comp

---
+++ lib/ios/Gamer/GMRActivityView.h
@@ -0,0 +1,16 @@
+//
+//  GMRActivityView.h
+//  Gamer
+//
+//  Created by Adam Venturella on 12/8/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GMRActivityView : UIView {
+
+}
+
+@end
---
+++ lib/ios/Gamer/GMRActivityView.m
@@ -0,0 +1,37 @@
+//
+//  GMRActivityView.m
+//  Gamer
+//
+//  Created by Adam Venturella on 12/8/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GMRActivityView.h"
+
+
+@implementation GMRActivityView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
