8906a64a62bb5fb2d9a26153a14b48a854d9ed2a
Adam Venturella <aventurella@gmail.com>
2011-01-02 10:54:43 -0800

    more copy refinements

--- www/index.php
+++ www/index.php
@@ -23,7 +23,8 @@ GMRPage::Controller('IndexController.php');
 			<div id="body">
 				<p>Any multiplayer game worth it's salt provides some kind of matchmaking. Which is great, especially if you enjoy your sexual orientation being called into question by a 10 year old. Or maybe you'd rather they go on and on about their exploits with the matriarch of your family; GamePop to the rescue!</p>
 				<p>You probably have friends, we hope.  Chances are you are also part of at least one social network, Twitter, Facebook, etc, etc. Let your friends and communities know you are up for throwing one down with GamePop.</p>
-				<p>To be sure, if you just want a quick game, in-game matchmaking is still probably the route to go. If you have a more of a life and need to plan your games in advance, GamePop is for you.</p>
+				<p>To be sure, if you just want a quick game, in-game matchmaking is still probably the route to go.</p>
+				<p>You probably have more fun playing with your friends though, so get 'em all together with GamePop.</p>
 			</div>
 			<div id="copyright">Copyright 2011 Happy Gravity Media</div>
 		</div>
